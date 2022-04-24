74a37e54d0807338669d97aed351df25ac870826
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-04-25 00:17:56 +0200

    Print generated OpenAPI json

--- src/main/kotlin/dev/arli/openapi/OpenAPIGen.kt
+++ src/main/kotlin/dev/arli/openapi/OpenAPIGen.kt
@@ -1,5 +1,9 @@
 package dev.arli.openapi
 
+import dev.arli.openapi.generator.ContactJsonGenerator
+import dev.arli.openapi.generator.InfoJsonGenerator
+import dev.arli.openapi.generator.LicenseJsonGenerator
+import dev.arli.openapi.generator.OpenAPIJsonGenerator
 import io.ktor.server.application.ApplicationStarted
 import io.ktor.server.application.createApplicationPlugin
 import io.ktor.server.application.hooks.MonitoringEvent
@@ -8,7 +12,19 @@ val OpenAPIGen = createApplicationPlugin(
     name = "OpenAPIGen",
     createConfiguration = ::OpenAPIGenConfiguration
 ) {
+    val contactJsonGenerator = ContactJsonGenerator()
+    val licenseJsonGenerator = LicenseJsonGenerator()
+    val infoJsonGenerator = InfoJsonGenerator(
+        contactJsonGenerator = contactJsonGenerator,
+        licenseJsonGenerator = licenseJsonGenerator
+    )
+    val openAPIJsonGenerator = OpenAPIJsonGenerator(
+        infoJsonGenerator = infoJsonGenerator
+    )
+
     on(MonitoringEvent(ApplicationStarted)) {
-        println(">>> OpenAPIGen installed: $pluginConfig")
+        println(">>> OpenAPIGen installed")
+        println(">>> Configuration: $pluginConfig")
+        println(">>> OpenAPI JSON: ${openAPIJsonGenerator.generate(pluginConfig)}")
     }
 }
