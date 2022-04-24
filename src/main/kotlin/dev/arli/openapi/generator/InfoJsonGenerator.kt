a0bbefba172944def22ebfb3a7699c6214be936f
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-04-25 00:13:39 +0200

    Add InfoJsonGenerator

---
+++ src/main/kotlin/dev/arli/openapi/generator/InfoJsonGenerator.kt
@@ -0,0 +1,31 @@
+package dev.arli.openapi.generator
+
+import dev.arli.openapi.model.InfoObject
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+class InfoJsonGenerator(
+    private val contactJsonGenerator: ContactJsonGenerator,
+    private val licenseJsonGenerator: LicenseJsonGenerator
+) {
+
+    fun generateInfoJson(info: InfoObject): JsonObject {
+        return buildJsonObject {
+            put("title", info.title)
+            info.description?.let { description ->
+                put("description", description)
+            }
+            put("version", info.version)
+            info.termsOfService?.let { termsOfService ->
+                put("termsOfService", termsOfService.toString())
+            }
+            info.contact?.let { contact ->
+                put("contact", contactJsonGenerator.generateContactJson(contact))
+            }
+            info.license?.let { license ->
+                put("license", licenseJsonGenerator.generateLicenseJson(license))
+            }
+        }
+    }
+}
