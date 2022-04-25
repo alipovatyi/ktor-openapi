67ebaad92ee70e35199b3f8f6e81513a3db15b1f
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-04-25 15:18:06 +0200

    Update sample application configuration

--- sample/src/main/kotlin/dev/arli/openapi/sample/SampleApplication.kt
+++ sample/src/main/kotlin/dev/arli/openapi/sample/SampleApplication.kt
@@ -1,6 +1,7 @@
 package dev.arli.openapi.sample
 
 import dev.arli.openapi.OpenAPIGen
+import io.ktor.http.Url
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
@@ -18,8 +19,30 @@ fun main() {
         }
         install(OpenAPIGen) {
             info {
-                title = "Swagger Petstore"
-                version = "0.0.1"
+                title = "Swagger Petstore - OpenAPI 3.0"
+                version = "1.0.11"
+                description = """
+                    This is a sample Pet Store Server based on the OpenAPI 3.0 specification. You can find out more
+                    about Swagger at [https://swagger.io](https://swagger.io). In the third iteration of the pet store,
+                    we've switched to the design first approach!
+                    You can now help us improve the API whether it's by making changes to the definition itself or to 
+                    the code.
+                    That way, with time, we can improve the API in general, and expose some of the new features in OAS3.
+                    
+                    Some useful links:
+                    - [The Pet Store repository](https://github.com/swagger-api/swagger-petstore)
+                    - [The source API definition for the Pet Store](https://github.com/swagger-api/swagger-petstore/blob/master/src/main/resources/openapi.yaml)
+                """.trimIndent()
+                termsOfService = Url("https://swagger.io/terms/")
+
+                contact {
+                    email = "apiteam@swagger.io"
+                }
+
+                license {
+                    name = "Apache 2.0"
+                    url = Url("https://www.apache.org/licenses/LICENSE-2.0.html")
+                }
             }
         }
     }.start(wait = true)
