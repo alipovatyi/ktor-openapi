ff1dce136ed4bdef07ea0a704b03c3c87812a8ce
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-05-09 18:21:05 +0200

    Add base PathItemObject

---
+++ src/main/kotlin/dev/arli/openapi/model/OperationObject.kt
@@ -0,0 +1,18 @@
+package dev.arli.openapi.model
+
+import io.ktor.http.HttpStatusCode
+
+data class OperationObject(
+    val tags: Set<String> = emptySet(),
+    val summary: String? = null,
+    val description: String? = null,
+    val externalDocs: ExternalDocumentationObject? = null,
+    val operationId: String? = null,
+    val parameters: List<ParameterComponent> = emptyList(),
+    val requestBody: RequestBodyComponent? = null,
+    val responses: Map<HttpStatusCode, ResponseComponent> = emptyMap(), // REQUIRED // TODO handle `default`
+//    val callbacks: Map<String, CallbackComponent> = emptyMap(), // TODO
+    val deprecated: Boolean = false,
+//    val security: List<SecurityRequirementObject> = emptyList(), // TODO
+//    val servers: List<ServerObject> = emptyList() // TODO
+)
---
+++ src/main/kotlin/dev/arli/openapi/model/SecurityRequirementObject.kt
@@ -0,0 +1,4 @@
+package dev.arli.openapi.model
+
+@JvmInline
+value class SecurityRequirementObject(val value: String)
