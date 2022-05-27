648cdd03511a9d9095788afd9f5c7cf601fec401
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-05-27 22:27:59 +0200

    Add NumberFormat

---
+++ src/main/kotlin/dev/arli/openapi/util/NumberFormat.kt
@@ -0,0 +1,22 @@
+package dev.arli.openapi.util
+
+import kotlin.reflect.KType
+
+enum class NumberFormat(val key: String) {
+    NO_FORMAT(""),
+    INT_32("int32"),
+    INT_64("int64"),
+    FLOAT("float"),
+    DOUBLE("double")
+}
+
+fun KType.getNumberFormat(): NumberFormat {
+    return when (this) {
+        DataTypes.intType -> NumberFormat.INT_32
+        DataTypes.longType -> NumberFormat.INT_64
+        DataTypes.floatType-> NumberFormat.FLOAT
+        DataTypes.doubleType-> NumberFormat.DOUBLE
+        DataTypes.bigDecimalType-> NumberFormat.NO_FORMAT
+        else -> throw IllegalArgumentException("Unsupported number type [$this]")
+    }
+}
---
+++ src/test/kotlin/dev/arli/openapi/util/NumberFormatTest.kt
@@ -0,0 +1,41 @@
+package dev.arli.openapi.util
+
+import java.math.BigDecimal
+import kotlin.reflect.KType
+import kotlin.reflect.typeOf
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class NumberFormatTest {
+
+    @ParameterizedTest
+    @MethodSource
+    fun `Should return number format`(givenType: KType, expectedNumberFormat: NumberFormat) {
+        assertEquals(expectedNumberFormat, givenType.getNumberFormat())
+    }
+
+    @Test
+    fun `Should throw exception for unsupported number type`() {
+        val givenType = typeOf<String>()
+
+        assertFailsWith<IllegalArgumentException> {
+            givenType.getNumberFormat()
+        }
+    }
+
+    private companion object{
+
+        @JvmStatic
+        fun `Should return number format`() = listOf(
+            arguments(typeOf<Int>(), NumberFormat.INT_32),
+            arguments(typeOf<Long>(), NumberFormat.INT_64),
+            arguments(typeOf<Double>(), NumberFormat.DOUBLE),
+            arguments(typeOf<Float>(), NumberFormat.FLOAT),
+            arguments(typeOf<BigDecimal>(), NumberFormat.NO_FORMAT)
+        )
+    }
+}
