b5ff2d6f86710ec6ce8781478c6eba967f16642c
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-05-28 13:55:51 +0200

    Add integer property mapper

---
+++ src/main/kotlin/dev/arli/openapi/mapper/IntegerPropertyMapper.kt
@@ -0,0 +1,28 @@
+package dev.arli.openapi.mapper
+
+import dev.arli.openapi.model.IntegerProperty
+import dev.arli.openapi.parser.DescriptionParser
+import dev.arli.openapi.parser.PropertyNameParser
+import dev.arli.openapi.util.DataType
+import dev.arli.openapi.util.getDataType
+import dev.arli.openapi.util.getNumberFormat
+import kotlin.reflect.KProperty
+
+class IntegerPropertyMapper(
+    private val propertyNameParser: PropertyNameParser = PropertyNameParser(),
+    private val descriptionParser: DescriptionParser = DescriptionParser()
+) {
+
+    fun map(property: KProperty<*>): IntegerProperty {
+        require(property.returnType.getDataType() == DataType.INTEGER) {
+            "Property [$property] should be of an integer data type"
+        }
+
+        return IntegerProperty(
+            name = propertyNameParser.parse(property),
+            description = descriptionParser.parse(property),
+            nullable = property.returnType.isMarkedNullable,
+            format = property.returnType.getNumberFormat()
+        )
+    }
+}
---
+++ src/test/kotlin/dev/arli/openapi/mapper/IntegerPropertyMapperTest.kt
@@ -0,0 +1,121 @@
+package dev.arli.openapi.mapper
+
+import dev.arli.openapi.annotation.Description
+import dev.arli.openapi.model.IntegerProperty
+import dev.arli.openapi.util.NumberFormat
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlinx.serialization.SerialName
+import org.junit.jupiter.api.Test
+
+internal class IntegerPropertyMapperTest {
+
+    private val mapper = IntegerPropertyMapper()
+
+    @Test
+    fun `Should map Int property to integer property`() {
+        val givenProperty = TestClassWithInt::value
+
+        val expectedIntegerProperty = IntegerProperty(
+            name = "value",
+            description = null,
+            nullable = false,
+            format = NumberFormat.INT_32
+        )
+
+        assertEquals(expectedIntegerProperty, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should map nullable Int property to nullable integer property`() {
+        val givenProperty = TestClassWithNullableInt::value
+
+        val expectedIntegerProperty = IntegerProperty(
+            name = "value",
+            description = null,
+            nullable = true,
+            format = NumberFormat.INT_32
+        )
+
+        assertEquals(expectedIntegerProperty, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should map Long property to integer property`() {
+        val givenProperty = TestClassWithLong::value
+
+        val expectedIntegerProperty = IntegerProperty(
+            name = "value",
+            description = null,
+            nullable = false,
+            format = NumberFormat.INT_64
+        )
+
+        assertEquals(expectedIntegerProperty, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should map nullable Long property to nullable integer property`() {
+        val givenProperty = TestClassWithNullableLong::value
+
+        val expectedIntegerProperty = IntegerProperty(
+            name = "value",
+            description = null,
+            nullable = true,
+            format = NumberFormat.INT_64
+        )
+
+        assertEquals(expectedIntegerProperty, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should map property to integer property with custom name`() {
+        val givenProperty = TestClassWithCustomSerialName::value
+
+        val expectedIntegerProperty = IntegerProperty(
+            name = "custom-value",
+            description = null,
+            nullable = false,
+            format = NumberFormat.INT_32
+        )
+
+        assertEquals(expectedIntegerProperty, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should map property to integer property with description`() {
+        val givenProperty = TestClassWithDescription::value
+
+        val expectedIntegerProperty = IntegerProperty(
+            name = "value",
+            description = "Description",
+            nullable = false,
+            format = NumberFormat.INT_32
+        )
+
+        assertEquals(expectedIntegerProperty, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should throw exception for unsupported integer type`() {
+        val givenProperty = TestClassWithInvalidType::value
+
+        assertFailsWith<IllegalArgumentException> {
+            mapper.map(givenProperty)
+        }
+    }
+
+    private data class TestClassWithInt(val value: Int)
+
+    private data class TestClassWithNullableInt(val value: Int?)
+
+    private data class TestClassWithLong(val value: Long)
+
+    private data class TestClassWithNullableLong(val value: Long?)
+
+    private data class TestClassWithCustomSerialName(@SerialName("custom-value") val value: Int)
+
+    private data class TestClassWithDescription(@Description("Description") val value: Int)
+
+    private data class TestClassWithInvalidType(val value: Float)
+}
