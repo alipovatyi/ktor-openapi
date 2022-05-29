a511adba336da7eff0822e46885a46f040f87b58
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-05-29 20:47:54 +0200

    Implement parameter json generator

--- src/main/kotlin/dev/arli/openapi/generator/ParameterJsonGenerator.kt
+++ src/main/kotlin/dev/arli/openapi/generator/ParameterJsonGenerator.kt
@@ -1,13 +1,29 @@
 package dev.arli.openapi.generator
 
 import dev.arli.openapi.model.ParameterComponent
+import dev.arli.openapi.model.ParameterObject
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
-// TODO
-class ParameterJsonGenerator {
+class ParameterJsonGenerator(
+    private val schemaJsonGenerator: SchemaJsonGenerator = SchemaJsonGenerator()
+) {
 
     fun generateParameterJson(parameter: ParameterComponent): JsonObject {
-        return buildJsonObject {}
+        return buildJsonObject {
+            if (parameter is ParameterObject) {
+                put("name", parameter.name)
+                put("in", parameter.`in`.key)
+                parameter.description?.let { description ->
+                    put("description", description)
+                }
+                put("required", parameter.required)
+                put("deprecated", parameter.deprecated)
+                parameter.schema?.let { schema ->
+                    put("schema", schemaJsonGenerator.generateSchemaJson(schema))
+                }
+            }
+        }
     }
 }
