7181dc1015ceff72b7346cecc8c75dcdc3a5ce7e
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-06-01 00:44:14 +0200

    Add cookie parameter name parser

---
+++ src/main/kotlin/dev/arli/openapi/parser/CookieParameterNameParser.kt
@@ -0,0 +1,17 @@
+package dev.arli.openapi.parser
+
+import dev.arli.openapi.annotation.Cookie
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.findAnnotation
+
+class CookieParameterNameParser {
+
+    fun parse(property: KProperty<*>): String {
+        val propertyName = property.name
+        val cookieAnnotation = requireNotNull(property.findAnnotation<Cookie>()) {
+            "Cookie parameter [$propertyName] must be annotated with @Cookie annotation"
+        }
+        val cookieAnnotationName = cookieAnnotation.name.takeIf { it.isNotBlank() }
+        return cookieAnnotationName ?: propertyName
+    }
+}
