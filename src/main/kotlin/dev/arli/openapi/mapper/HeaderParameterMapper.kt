a888760efbe60de1c7043d9e32a9e047663585be
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-05-31 23:46:52 +0200

    Add header parameter mapper

---
+++ src/main/kotlin/dev/arli/openapi/mapper/HeaderParameterMapper.kt
@@ -0,0 +1,31 @@
+package dev.arli.openapi.mapper
+
+import dev.arli.openapi.annotation.Header
+import dev.arli.openapi.model.ParameterComponent
+import dev.arli.openapi.model.ParameterLocation
+import dev.arli.openapi.model.ParameterObject
+import dev.arli.openapi.parser.HeaderParameterNameParser
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.findAnnotation
+
+class HeaderParameterMapper(
+    private val headerParameterNameParser: HeaderParameterNameParser = HeaderParameterNameParser(),
+    private val schemaComponentMapper: SchemaComponentMapper = SchemaComponentMapper()
+) {
+
+    fun map(property: KProperty<*>): ParameterComponent {
+        val name = headerParameterNameParser.parse(property)
+        val headerAnnotation = requireNotNull(property.findAnnotation<Header>()) {
+            "Header parameter [$name] must be annotated with @Header annotation"
+        }
+
+        return ParameterObject(
+            name = name,
+            `in` = ParameterLocation.HEADER,
+            required = headerAnnotation.required,
+            description = headerAnnotation.description,
+            deprecated = headerAnnotation.deprecated,
+            schema = schemaComponentMapper.map(property)
+        )
+    }
+}
