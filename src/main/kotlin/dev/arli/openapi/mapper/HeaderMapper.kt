f172510e130620ad90e8db0e0ba2dd9b3814d922
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-06-01 16:20:05 +0200

    Add header mapper

---
+++ src/main/kotlin/dev/arli/openapi/mapper/HeaderMapper.kt
@@ -0,0 +1,28 @@
+package dev.arli.openapi.mapper
+
+import dev.arli.openapi.annotation.Header
+import dev.arli.openapi.model.HeaderComponent
+import dev.arli.openapi.model.HeaderObject
+import dev.arli.openapi.parser.HeaderNameParser
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.findAnnotation
+
+class HeaderMapper(
+    private val headerNameParser: HeaderNameParser = HeaderNameParser(),
+    private val schemaComponentMapper: SchemaComponentMapper = SchemaComponentMapper()
+) {
+
+    fun map(property: KProperty<*>): HeaderComponent {
+        val name = headerNameParser.parse(property)
+        val headerAnnotation = requireNotNull(property.findAnnotation<Header>()) {
+            "Header [$name] must be annotated with @Header annotation"
+        }
+
+        return HeaderObject(
+            description = headerAnnotation.description,
+            required = headerAnnotation.required,
+            deprecated = headerAnnotation.deprecated,
+            schema = schemaComponentMapper.map(property)
+        )
+    }
+}
---
+++ src/test/kotlin/dev/arli/openapi/mapper/HeaderMapperTest.kt
@@ -0,0 +1,75 @@
+package dev.arli.openapi.mapper
+
+import dev.arli.openapi.annotation.Header
+import dev.arli.openapi.model.HeaderObject
+import dev.arli.openapi.model.SchemaObject
+import dev.arli.openapi.model.property.DataType
+import dev.arli.openapi.model.property.StringFormat
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import org.junit.jupiter.api.Test
+
+internal class HeaderMapperTest {
+
+    private val mapper = HeaderMapper()
+
+    @Test
+    fun `Should map header with default values to header object`() {
+        val givenProperty = TestClassWithDefaultValues::value
+
+        val expectedHeaderObject = HeaderObject(
+            required = false,
+            description = "",
+            deprecated = false,
+            schema = SchemaObject(
+                type = DataType.STRING,
+                format = StringFormat.NO_FORMAT,
+                nullable = false,
+                description = null,
+                properties = emptyMap(),
+                enum = emptySet()
+            )
+        )
+
+        assertEquals(expectedHeaderObject, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should map header with custom values to header object`() {
+        val givenProperty = TestClassWithCustomValues::value
+
+        val expectedHeaderObject = HeaderObject(
+            required = true,
+            description = "Description",
+            deprecated = true,
+            schema = SchemaObject(
+                type = DataType.STRING,
+                format = StringFormat.NO_FORMAT,
+                nullable = false,
+                description = null,
+                properties = emptyMap(),
+                enum = emptySet()
+            )
+        )
+
+        assertEquals(expectedHeaderObject, mapper.map(givenProperty))
+    }
+
+    @Test
+    fun `Should throw exception if Header annotation is missing`() {
+        val givenProperty = TestClassWithoutAnnotation::value
+
+        assertFailsWith<IllegalArgumentException> {
+            mapper.map(givenProperty)
+        }
+    }
+
+    private data class TestClassWithDefaultValues(@Header val value: String)
+
+    private data class TestClassWithCustomValues(
+        @Header(name = "custom-value", description = "Description", required = true, deprecated = true)
+        val value: String
+    )
+
+    private data class TestClassWithoutAnnotation(val value: Long)
+}
