9fd555f4ae3cb3dc6ed7c8ba4fe213187687b084
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-07-27 22:13:38 +0200

    Update Kotlin and dependencies to the latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
-    id("org.jetbrains.kotlinx.kover") version "0.5.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+    id("org.jetbrains.kotlinx.kover") version "0.5.1"
 }
 
 allprojects {
