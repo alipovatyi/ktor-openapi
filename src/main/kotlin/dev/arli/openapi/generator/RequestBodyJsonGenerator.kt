aea90daa56bc2207f2b0e60aec8ab598993ff5fb
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-07-30 21:39:19 +0200

    Implement RequestBodyJsonGenerator

--- src/main/kotlin/dev/arli/openapi/generator/RequestBodyJsonGenerator.kt
+++ src/main/kotlin/dev/arli/openapi/generator/RequestBodyJsonGenerator.kt
@@ -1,13 +1,29 @@
 package dev.arli.openapi.generator
 
 import dev.arli.openapi.model.RequestBodyComponent
+import dev.arli.openapi.model.RequestBodyObject
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonObject
 
-// TODO
-class RequestBodyJsonGenerator {
+class RequestBodyJsonGenerator(
+    private val mediaTypeJsonGenerator: MediaTypeJsonGenerator = MediaTypeJsonGenerator()
+) {
 
     fun generateRequestBodyJson(requestBody: RequestBodyComponent): JsonObject {
-        return buildJsonObject {}
+        return buildJsonObject {
+            if (requestBody is RequestBodyObject<*>) {
+                requestBody.description?.let { description ->
+                    put("description", description)
+                }
+                putJsonObject("content") {
+                    requestBody.content.forEach { (mediaType, mediaTypeObject) ->
+                        put(mediaType.key, mediaTypeJsonGenerator.generateMediaTypeJson(mediaType, mediaTypeObject))
+                    }
+                }
+                put("required", requestBody.required)
+            }
+        }
     }
 }
--- src/test/kotlin/dev/arli/openapi/generator/RequestBodyJsonGeneratorTest.kt
+++ src/test/kotlin/dev/arli/openapi/generator/RequestBodyJsonGeneratorTest.kt
@@ -1,19 +1,129 @@
 package dev.arli.openapi.generator
 
+import com.google.common.truth.Truth.assertThat
+import dev.arli.openapi.model.MediaType
+import dev.arli.openapi.model.MediaTypeObject
+import dev.arli.openapi.model.RequestBodyObject
+import dev.arli.openapi.model.SchemaObject
+import dev.arli.openapi.model.property.DataType
+import dev.arli.openapi.model.property.StringFormat
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonObject
 import org.junit.jupiter.api.Test
 
-// TODO
 internal class RequestBodyJsonGeneratorTest {
 
     private val generator = RequestBodyJsonGenerator()
 
     @Test
     fun `Should convert request body object to json object`() {
+        val givenRequestBodyObject = RequestBodyObject(
+            description = "Description",
+            content = mapOf(
+                MediaType.APPLICATION_JSON to MediaTypeObject(
+                    schema = SchemaObject(
+                        type = DataType.OBJECT,
+                        format = null,
+                        nullable = false,
+                        properties = mapOf(
+                            "property" to SchemaObject(
+                                type = DataType.STRING,
+                                format = StringFormat.DATE,
+                                nullable = true
+                            )
+                        )
+                    ),
+                    example = TestContent("Example"),
+                    exampleJson = buildJsonObject { put("value", "Example") },
+                    examples = emptyMap()
+                )
+            ),
+            required = true
+        )
 
+        val expectedJsonObject = buildJsonObject {
+            put("description", "Description")
+            putJsonObject("content") {
+                putJsonObject("application/json") {
+                    putJsonObject("schema") {
+                        put("type", "object")
+                        put("nullable", false)
+                        putJsonObject("properties") {
+                            putJsonObject("property") {
+                                put("type", "string")
+                                put("format", "date")
+                                put("nullable", true)
+                            }
+                        }
+                    }
+                    putJsonObject("example") {
+                        put("value", "Example")
+                    }
+                }
+            }
+            put("required", true)
+        }
+
+        val actualJsonObject = generator.generateRequestBodyJson(givenRequestBodyObject)
+
+        assertThat(actualJsonObject).isEqualTo(expectedJsonObject)
+    }
+
+    @Test
+    fun `Should exclude null values`() {
+        val givenRequestBodyObject = RequestBodyObject(
+            description = null,
+            content = mapOf(
+                MediaType.APPLICATION_JSON to MediaTypeObject(
+                    schema = SchemaObject(
+                        type = DataType.OBJECT,
+                        format = null,
+                        nullable = false,
+                        properties = mapOf(
+                            "property" to SchemaObject(
+                                type = DataType.STRING,
+                                format = StringFormat.DATE,
+                                nullable = true
+                            )
+                        )
+                    ),
+                    example = null,
+                    exampleJson = null,
+                    examples = emptyMap()
+                )
+            ),
+            required = false
+        )
+
+        val expectedJsonObject = buildJsonObject {
+            putJsonObject("content") {
+                putJsonObject("application/json") {
+                    putJsonObject("schema") {
+                        put("type", "object")
+                        put("nullable", false)
+                        putJsonObject("properties") {
+                            putJsonObject("property") {
+                                put("type", "string")
+                                put("format", "date")
+                                put("nullable", true)
+                            }
+                        }
+                    }
+                }
+            }
+            put("required", false)
+        }
+
+        val actualJsonObject = generator.generateRequestBodyJson(givenRequestBodyObject)
+
+        assertThat(actualJsonObject).isEqualTo(expectedJsonObject)
     }
 
     @Test
     fun `Should convert reference object to json object`() {
-
+        // TODO
     }
+
+    private data class TestContent(val value: String)
 }
