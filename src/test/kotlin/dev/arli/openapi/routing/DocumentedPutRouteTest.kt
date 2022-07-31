ae67812fa7ea040e4330edb8269740644b9f00f5
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-07-31 23:40:01 +0200

    Add documented PUT route

---
+++ src/test/kotlin/dev/arli/openapi/routing/DocumentedPutRouteTest.kt
@@ -0,0 +1,11 @@
+package dev.arli.openapi.routing
+
+import org.junit.jupiter.api.Test
+
+internal class DocumentedPutRouteTest {
+
+    @Test
+    fun `Should set up documented PUT route`() {
+        // TODO
+    }
+}
