20ee71cad6a750fe9ea346f3ebe8a591005cdaf9
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-08-07 00:08:13 +0200

    Update Route::getPath implementation

--- src/main/kotlin/dev/arli/openapi/util/RouteExtension.kt
+++ src/main/kotlin/dev/arli/openapi/util/RouteExtension.kt
@@ -1,7 +1,24 @@
 package dev.arli.openapi.util
 
+import io.ktor.server.auth.AuthenticationRouteSelector
+import io.ktor.server.routing.HttpMethodRouteSelector
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.TrailingSlashRouteSelector
 
 fun Route?.getPath(): String {
-    return this?.parent?.toString()?.removeTrailingSlash() ?: "/"
+    if (this == null) return  "/"
+    return when (val parentRoute = parent?.getPath()) {
+        null -> when (selector) {
+            is AuthenticationRouteSelector,
+            is HttpMethodRouteSelector,
+            is TrailingSlashRouteSelector -> "/"
+            else -> "/$selector"
+        }
+        else -> when (selector) {
+            is AuthenticationRouteSelector,
+            is HttpMethodRouteSelector,
+            is TrailingSlashRouteSelector -> if (parentRoute.endsWith('/')) parentRoute else "$parentRoute/"
+            else -> if (parentRoute.endsWith('/')) "$parentRoute$selector" else "$parentRoute/$selector"
+        }
+    }.removeTrailingSlash()
 }
--- src/test/kotlin/dev/arli/openapi/util/RouteExtensionTest.kt
+++ src/test/kotlin/dev/arli/openapi/util/RouteExtensionTest.kt
@@ -1,8 +1,10 @@
 package dev.arli.openapi.util
 
+import com.google.common.truth.Truth.assertThat
 import io.ktor.server.routing.Route
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 internal class RouteExtensionTest {
@@ -10,12 +12,15 @@ internal class RouteExtensionTest {
     @ParameterizedTest
     @MethodSource
     fun `Should return full route path`(givenRoute: Route?, expected: String) {
-        // TODO
+        assertThat(givenRoute.getPath()).isEqualTo(expected)
     }
 
     private companion object {
 
         @JvmStatic
-        fun `Should return full route path`() = listOf<Arguments>()
+        fun `Should return full route path`() = listOf<Arguments>(
+            arguments(null, "/"),
+            // TODO: add more cases
+        )
     }
 }
