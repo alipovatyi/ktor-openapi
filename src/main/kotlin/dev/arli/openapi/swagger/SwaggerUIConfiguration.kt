4159954902e0d7e7de9c411edbae1cd8264b24c5
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-08-24 23:49:16 +0200

    Add SwaggerUIConfiguration builder

--- src/main/kotlin/dev/arli/openapi/swagger/SwaggerUIConfiguration.kt
+++ src/main/kotlin/dev/arli/openapi/swagger/SwaggerUIConfiguration.kt
@@ -1,11 +1,26 @@
 package dev.arli.openapi.swagger
 
+typealias SwaggerUIConfigurationBuilder = SwaggerUIConfiguration.Builder.() -> Unit
+
 data class SwaggerUIConfiguration(
-    var path: String = "/",
-    var webjarsPath: String = "webjars",
+    val path: String = "/",
+    val webjarsPath: String = "webjars",
     internal val specificationFileName: String
 ) {
     val oauth2RedirectPath = "/$path/$OAUTH2_REDIRECT_PATH".replace("//+".toRegex(), "/")
+
+    class Builder(private val specificationFileName: String) {
+        var path: String = "/"
+        var webjarsPath: String = "webjars"
+
+        fun build(): SwaggerUIConfiguration {
+            return SwaggerUIConfiguration(
+                path = path,
+                webjarsPath = webjarsPath,
+                specificationFileName = specificationFileName
+            )
+        }
+    }
 }
 
 private const val OAUTH2_REDIRECT_PATH = "oauth2-redirect"
--- src/test/kotlin/dev/arli/openapi/swagger/SwaggerUIConfigurationTest.kt
+++ src/test/kotlin/dev/arli/openapi/swagger/SwaggerUIConfigurationTest.kt
@@ -9,6 +9,24 @@ import org.junit.jupiter.params.provider.MethodSource
 internal class SwaggerUIConfigurationTest {
 
     @Test
+    fun `Should create tag`() {
+        val expectedSwaggerUIConfiguration = SwaggerUIConfiguration(
+            path = "path",
+            webjarsPath = "webjars-path",
+            specificationFileName = "specification.json"
+        )
+
+        val actualSwaggerUIConfiguration = SwaggerUIConfiguration.Builder(
+            specificationFileName = "specification.json"
+        ).apply {
+            path = "path"
+            webjarsPath = "webjars-path"
+        }.build()
+
+        assertThat(actualSwaggerUIConfiguration).isEqualTo(expectedSwaggerUIConfiguration)
+    }
+
+    @Test
     fun `Should return swagger ui configuration with default values`() {
         val expectedSwaggerUIConfiguration = SwaggerUIConfiguration(
             path = "/",
