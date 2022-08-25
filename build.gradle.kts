b6b679a6814b4bdcd3d84d450d2fbdb63d875c7b
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-08-25 11:31:49 +0200

    Add Gradle versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.serialization") version "1.7.10"
+    id("com.github.ben-manes.versions") version "0.42.0"
     id("org.jetbrains.kotlinx.kover") version "0.5.1"
     id("com.diffplug.spotless") version "6.7.2"
 }
