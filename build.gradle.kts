93fd72b8495ec199908af4fb9c9d7f86803b2792
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-12-31 00:00:51 +0100

    Bump kotlin version from 1.7.20 to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
     id("com.github.ben-manes.versions") version "0.44.0"
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
     id("com.diffplug.spotless") version "6.12.0"
