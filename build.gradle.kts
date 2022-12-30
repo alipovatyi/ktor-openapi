a3147615fa913698bea3b62936539454389ce37c
Artem Lipovatyi <artemlipovatyi@gmail.com>
2022-12-30 23:43:51 +0100

    Bump version plugin version from 0.43.0 to 0.44.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
-    id("com.github.ben-manes.versions") version "0.43.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
     id("org.jetbrains.kotlinx.kover") version "0.5.1"
     id("com.diffplug.spotless") version "6.12.0"
     id("maven-publish")
